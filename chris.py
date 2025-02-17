b06e25bdfb5ae3b8b2738c8ae4c2cb08b6dda030
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-18 00:57:32 +0800

    update

--- chris.py
+++ chris.py
@@ -17,17 +17,27 @@ GPIO.setup(STATION_3, GPIO.IN)
 
 # Callback function for station detection
 def station_callback(channel):
-    if channel == STATION_1:
-        print("Station 1 detected!")
-    elif channel == STATION_2:
-        print("Station 2 detected!")
-    elif channel == STATION_3:
-        print("Station 3 detected!")
+    if GPIO.input(channel) == GPIO.LOW:
+        # Train is arriving (falling edge)
+        if channel == STATION_1:
+            print("Train ARRIVED at Station 1")
+        elif channel == STATION_2:
+            print("Train ARRIVED at Station 2")
+        elif channel == STATION_3:
+            print("Train ARRIVED at Station 3")
+    else:
+        # Train is leaving (rising edge)
+        if channel == STATION_1:
+            print("Train LEFT Station 1")
+        elif channel == STATION_2:
+            print("Train LEFT Station 2")
+        elif channel == STATION_3:
+            print("Train LEFT Station 3")
 
-# Add event detection for each station
-GPIO.add_event_detect(STATION_1, GPIO.FALLING, callback=station_callback, bouncetime=300)
-GPIO.add_event_detect(STATION_2, GPIO.FALLING, callback=station_callback, bouncetime=300)
-GPIO.add_event_detect(STATION_3, GPIO.FALLING, callback=station_callback, bouncetime=300)
+# Add event detection for each station (both rising and falling edges)
+GPIO.add_event_detect(STATION_1, GPIO.BOTH, callback=station_callback, bouncetime=300)
+GPIO.add_event_detect(STATION_2, GPIO.BOTH, callback=station_callback, bouncetime=300)
+GPIO.add_event_detect(STATION_3, GPIO.BOTH, callback=station_callback, bouncetime=300)
 
 if __name__ == '__main__':
     try:
